1193050a43197c8c3a287e4c8019cfecbb3b9ef9
DimitarCvetkovski <cvetkovski_d@yahoo.com>
2024-07-20 22:22:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/member/skillsMember.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
